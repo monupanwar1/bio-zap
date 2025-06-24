25f02a653c8347eba1e6f0b42533d35a2398a35d
kunal <monupanwar5655@gmail.com>
2025-06-24 16:32:07 +0530

    footer

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+  return (
+    <footer className="text-center text-gray-500  border-t mx-auto py-4">
+      {new Date().getFullYear()} Bio-zap .All right reserved
+    </footer>
+  )
+}
