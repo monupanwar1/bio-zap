5fa7920fce3f04e02a7e6c3bc1414e58def00099
kunal <monupanwar5655@gmail.com>
2025-06-24 16:28:36 +0530

    navbar

--- components/Container.tsx
+++ components/Container.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 export default function Container({ children }: { children: ReactNode }) {
   return (
-    <div className="max-w-[1750px] mx-auto bg-zinc-100 px-4 border-l border-r border-gray-400  min-h-screen sm:px-6 lg:px-8 relative">
+    <div className="max-w-[1750px] mx-auto bg-white px-4 border-l border-r border-gray-400  min-h-screen sm:px-6 lg:px-8 relative shadow-lg">
       {children}
     </div>
   );
