10d271993ab8a106b0ecbb9fee511b3894354e27
kunal <monupanwar5655@gmail.com>
2025-06-25 14:37:28 +0530

    add link to landing page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function page() {
+  return (
+    <main className="min-h-screen p-10">
+      <section>
+        <h1>welcome to adminDashbaordPage</h1>
+      </section>
+    </main>
+  )
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,12 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className=" bg-white/30 border-b border-white/40 z-50 backdrop-blur-lg">
+    <nav className="backdrop-blur-3xl bg-white/50">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-16">
-        <h1 className="  text-xl font-bold text-neutral-900">RandLink</h1>
-        <Link className="text-blue-500 hover:underline" href="#">
+        <Link className='text-2xl font-bold text-neutral-900"' href="/">
+        Bio-Zap
+        </Link>
+        <Link className="text-blue-500 hover:underline" href="/dashboard">
           {' '}
           Dashboard
         </Link>
