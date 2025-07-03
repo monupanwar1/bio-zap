28721261f07c5f8189a8861d18c60d184ffd8c93
kunal <monupanwar5655@gmail.com>
2025-07-03 20:27:43 +0530

    ..

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,18 @@
-// middleware.ts
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
 
-const isPublicRoute = createRouteMatcher([
-  '/custom-signin',
-  '/custom-signup', // ✅ make this public too
-]);
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)']);
 
 export default clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth();
-
-  if (!isPublicRoute(req) && !userId) {
-    return NextResponse.redirect(new URL('/custom-signin', req.url));
+  if (!isPublicRoute(req)) {
+    await auth.protect();
   }
-
-  return NextResponse.next();
 });
 
 export const config = {
-  matcher: ['/((?!_next|.*\\..*|favicon.ico).*)', '/(api|trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
