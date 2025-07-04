a2c82ec12077873a53e51f2d18434734db4c74c7
kunal <monupanwar5655@gmail.com>
2025-07-04 21:47:59 +0530

    db change

--- prisma/migrations/20250704161124_init/migration.sql
+++ prisma/migrations/20250704161124_init/migration.sql
@@ -1,22 +1,28 @@
 -- CreateTable
 CREATE TABLE "Card" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "userId" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "avatarUrl" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Card_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Link" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "cardId" INTEGER NOT NULL,
     "label" TEXT NOT NULL,
     "url" TEXT NOT NULL,
     "icon" TEXT NOT NULL,
-    CONSTRAINT "Link_cardId_fkey" FOREIGN KEY ("cardId") REFERENCES "Card" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+
+    CONSTRAINT "Link_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Card_slug_key" ON "Card"("slug");
+
+-- AddForeignKey
+ALTER TABLE "Link" ADD CONSTRAINT "Link_cardId_fkey" FOREIGN KEY ("cardId") REFERENCES "Card"("id") ON DELETE CASCADE ON UPDATE CASCADE;
