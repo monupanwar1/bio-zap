e5d7a1c31546bef88cd82e3073120bde98c212e8
kunal <monupanwar5655@gmail.com>
2025-07-05 08:43:39 +0530

    layout edit

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,8 +2,8 @@ import { SignIn } from '@clerk/nextjs';
 
 export default function SignInPage() {
   return (
-    <main className="min-h-screen flex items-center justify-center dark:from-zinc-930 dark:to-zinc-800 p-6">
-      <div className="w-full max-w-md dark:bg-zinc-930 borderdark:border-zinc-700 rounded-xl  p-6">
+    <main className="flex items-center justify-center p-10 dark:from-zinc-930 dark:to-zinc-800">
+      <div className=" dark:bg-zinc-930 borderdark:border-zinc-700 rounded-xl ">
         <SignIn appearance={{ elements: { card: 'shadow-none' } }} />
       </div>
     </main>
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,8 +2,8 @@ import { SignUp } from '@clerk/nextjs';
 
 export default function SignUpPage() {
   return (
-    <main className="min-h-screen flex items-center justify-center dark:from-zinc-930 dark:to-zinc-800 p-6">
-      <div className="w-full max-w-md dark:bg-zinc-930 borderdark:border-zinc-700 rounded-xl  p-6">
+    <main className="flex items-center justify-center p-10 dark:from-zinc-930 dark:to-zinc-800">
+      <div className=" dark:bg-zinc-930 borderdark:border-zinc-700 rounded-xl ">
         <SignUp appearance={{ elements: { card: 'shadow-none' } }} />
       </div>
     </main>
